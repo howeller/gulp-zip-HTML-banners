23b361763d56ca539c3d7ba87fd130f2f8eb1798
Eric Howell <eric.howell@ogilvy.com>
2020-02-13 16:47:57 -0800

    Updated task to include source.zip

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # gulp-zip-HTML-banners
 Packaging HTML banner files for RedTrax CMS.
 
+
+## Dependencies
+- [**NodeJs** - latest stable release](https://nodejs.org/en/)
+- [**gulp** ~v.4.0.0](https://www.npmjs.com/package/gulp)
+
 ## Setup
 1) You will need to have [NodeJs](https://nodejs.org/en/), & [Gulp](https://www.npmjs.com/package/gulp) installed globally. Follow the [Quick Start guide](https://gulpjs.com/docs/en/getting-started/quick-start) to get setup. 
 
@@ -9,6 +14,23 @@ Packaging HTML banner files for RedTrax CMS.
 3) In Terminal - navigate to your project folder and run `npm install` to download all of the npm packages.
 
 4) Place each banner in a folder named with it's Redtrax name inside the `src` directory.  This task will create the `_final.zip` and a copy of the HTML file inside `dist`. Everything you need for direct upload to RedTrax.
+
+Your directory structure should look like this.
+
+```cli
+├── gulpfile.js
+├── package.json
+├── node_modules
+├── dist
+│   └── backups
+├── lib
+│   └── fsUtils.js
+└── src
+    └── banner_300x250
+```
+
+Each banner should go inside a folder named with it's RedTrax name. For example `banner_300x250` will get renamed with your RedTrax banner name. Add as many banner folders as you need. Everything inside `banners` will get processed in the task.
+
 
 ## Tasks
 
@@ -21,20 +43,4 @@ To package everything up for uploading to RedTrax:
 
 ```cli
 gulp
-```
-
-## Dependencies
-**npm**
-
-```cli
-merge-stream
-path
-gulp
-gulp-rename
-gulp-zip
-```
-
-**Custom utility**
-```cli
-fsUtils.js
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,40 @@ const merge = require('merge-stream'),
 // Custom modules & config
 const util = require('./lib/fsUtils');
 
-const paths = {
+const dir = {
 	src:'./src',
 	dist:'./dist'
 }
 
-const srcFolders = util.getFolders(paths.src);
+const srcFolders = util.getFolders(dir.src);
 
 // Gulp Tasks
 gulp.task('default', zipFiles);
 
-function zipFiles() {
+function zipFiles(cb) {
 	let task = srcFolders.map(function(folder) {
-		let _src = path.join(paths.src, folder),// Path source files
-				_files = _src+'/*.{css,html,js,json}', // Package banner files into the _final zip
-				_images = _src+'/images/**';// Path to production image files
+		let _src = path.join(dir.src, folder),// Path source files
+				_files = _src+'/*.{css,html,js,json,jpg,png,gif}', // Package banner files into the _final zip
+				_images = _src+'/images/**';
 
 		let _final =  gulp.src([_files,_images],{base:_src})
 			.pipe(zip('_final.zip'))
 			.pipe(rename(function(file){file.basename = folder + file.basename;}))
-			.pipe(gulp.dest(paths.dist));
+			.pipe(gulp.dest(dir.dist));
 
-		let _html =  gulp.src(path.join(paths.src, folder,'*.html'))
-			.pipe(gulp.dest(paths.dist));
+		let _html =  gulp.src(path.join(dir.src, folder,'*.html'))
+			.pipe(gulp.dest(dir.dist));
+		
+		/*
+			Uncomment below to make a _source.zip of all source files except node_modules 
+		*/
+		// let _source =  gulp.src(['*.{json,js,txt,md}', 'lib/**', _src+'/**/*', 'dist'],{base:'./'})
+		// .pipe(zip('_source.zip'))
+		// .pipe(rename(function(file){file.basename = folder + file.basename;}))
+		// .pipe(gulp.dest(dir.dist));
+		// return merge(_source, _final, _html)
+
 		return merge(_final, _html)
 	});
-	let lastStream = task[task.length-1];
-	return lastStream;
+	cb();
 };
